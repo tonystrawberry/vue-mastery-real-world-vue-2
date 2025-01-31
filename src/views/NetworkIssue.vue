f8b6dd690f9bad61a7ab14b626022e7427fcdd56
Tony Duong <tony.duong.102@gmail.com>
2025-01-31 19:11:27 +0900

    :sparkles: add 404 & network issue page

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Network Issue</h1>
+    <p>
+      There was an issue connecting to the network. Please check your connection
+      and try again.
+      <router-link to="/">Go Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>404 - Not Found</h1>
+    <p>
+      The {{ resource || 'resource' }} you are looking for does not exist. It
+      may have been removed, had its name changed, or is temporarily
+      unavailable.
+      <router-link to="/">Go Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style></style>
